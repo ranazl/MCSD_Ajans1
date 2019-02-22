9695bf7be39c6fb937cd5cc591462c7facc7b2f8
ranazl <43135137+ranazl@users.noreply.github.com>
2019-02-22 17:02:44 +0330

    MCSD

---
+++ Ajans/Controllers/AjansController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ajans.Controllers
+{
+    public class AjansController : Controller
+    {
+        // GET: Ajans
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
